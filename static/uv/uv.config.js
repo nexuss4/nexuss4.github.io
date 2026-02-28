8ee871fb3e4abe8db4098dc5487032743753b676
nexus <122535095+nexuss2@users.noreply.github.com>
2026-02-28 14:28:04 +0000

    changed bare

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/load/',
-    bare:'https://research.africa.com/ca/',
+    bare:'https://ir.wilburwilliams.com',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
