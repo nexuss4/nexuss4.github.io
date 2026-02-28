2dadcc2fd1b8f84236627c4beca2b520bcdd3215
nexus <122535095+nexuss2@users.noreply.github.com>
2026-02-28 16:49:20 +0000

    Update bare URL in uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/load/',
-    bare:'https://ir.wilburwilliams.com',
+    bare:'https://petezahgames.com/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
